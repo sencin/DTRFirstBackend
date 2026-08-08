5fce8108b6bc00592e550a7191dbf3442868d095
sencin <0xrepository@gmail.com>
2026-08-08 13:40:22 +0800

    first commit

---
+++ src/main/java/com/tensura/dtrsystem/DtrsystemApplication.java
@@ -0,0 +1,13 @@
+package com.tensura.dtrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtrsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtrsystemApplication.class, args);
+	}
+
+}
